d5bfc27b08caf6a92506aa6b7b38cb9192078891
lukec11 <me@lukec.me>
2019-12-08 16:01:59 -0500

    updated Spotify for duplicate checking
    Added duplicate checking with spotipy. Authored by @harbar20<https://github.com/harbar20>
    
    Signed-off-by: lukec11 <me@lukec.me>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # autoplaylist
 Uses all Youtube, Spotify, Tidal, and SoundCloud links sent in the #music channel in the Hack Club slack and updates the playlist on each service individually.
 
-Written in pure python3, created by [@lukec11](https://github.com/lukec11) and [@harbar20](https://github.com/harbar20).
+Written in pure Python3, created by [@lukec11](https://github.com/lukec11) and [@harbar20](https://github.com/harbar20).
 
 This project was created almost exclusively using [VS Code Live Share](https://marketplace.visualstudio.com/items?itemName=MS-vsliveshare.vsliveshare).
--- spotify.py
+++ spotify.py
@@ -12,8 +12,6 @@ with open("config/SPconfig.json") as f:
     spotifyPlaylistId = spotifyConfig["playlistID"]
     spotifyCtr = spotifyConfig["ctr"]
     spotifyUser = spotifyConfig["spotifyUser"]
-    
-
 
 def addToSpotify(uuid):
     uid = [uuid] #This converts the UID string into a list, because spotipy api only accepts inputs as lists.
@@ -25,23 +23,38 @@ def addToSpotify(uuid):
     else:
         ("Usage: {} username".format(sys.argv[0])) #This is entirely irrelevant, but the code doesn't run without it.
 
-    token = spotipy.util.prompt_for_user_token(spotifyUser,
+    token = spotipy.util.prompt_for_user_token(spotifyUser, #prompts for spotify user token, so it can access the api
                                                scope,
                                                client_id=spotifyClientId,
                                                client_secret=spotifyClientSecret,
                                                redirect_uri='http://localhost:9898/spotifyCallback'
                                                ) #Authorizes with Spotify using OAuth
 
-
     if token: #Adds song to playlist
         sp = spotipy.Spotify(auth=token)
         sp.trace = False #idk what this does but the docs told me to do it
-        results = sp.user_playlist_add_tracks(spotifyUser, 
+        
+        tracks = sp.user_playlist_tracks(spotifyUser, spotifyPlaylistId, limit=100) #Gets all tracks in the playlist
+        """
+        with open("playlists/spPlaylist.json", 'w') as f:
+            json.dump(tracks, f, indent=4)
+        """
+        for trackInfo in tracks["items"]:
+            id = "spotify:track:"+trackInfo["track"]["id"]
+            if uuid == id:
+                alreadyInPlaylist = True
+            else:
+                alreadyInPlaylist = False
+        if not alreadyInPlaylist:
+            results = sp.user_playlist_add_tracks(spotifyUser, 
                                               spotifyPlaylistId, 
                                               uid
                                               ) #Adds song to said playlist
-        print (results) #logs snapshot to console
-        print ("Added to spotify playlist.")
+            #print (results) #logs snapshot to console
+            print ("Added to spotify playlist.")
+        else:
+            print("I'm sorry, this song is already in the playlist.")
     else:
         print ("CATCH HERE! Couldn't get token.") 
-        
+
+addToSpotify("spotify:track:2g2a5kDeZexbUTD8abcvm6")
