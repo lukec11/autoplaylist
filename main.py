46613b6f40d10199decbeb52736e365ef06b263c
lukec11 <me@lukec.me>
2019-12-07 19:13:36 -0500

    improved spotify and updated slack.
    used spotipy library for spotify OAuth, and adding tracks to the playlist. Also improved slack in main, so that it would work and not error as often.

--- main.py
+++ main.py
@@ -7,19 +7,27 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 from urlextract import URLExtract
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+#import applemusicpy 
 
 from youtube import add_to_youtube, ytAuth
+from spotify import addToSpotify
 
 extractor = URLExtract()
+
+
 
 #Spotify config stuff
 with open("config/SPconfig.json") as f:
     spotifyConfig = json.load(f)
     
-    spotifyClientID = spotifyConfig["clientID"]
+    spotifyClientId = spotifyConfig["clientID"]
+    spotifyClientSecret = spotifyConfig["clientSecret"]
     spotifyBearer = spotifyConfig["bearer"]
     spotifyPlaylistId = spotifyConfig["playlistID"]
     spotifyCtr = spotifyConfig["ctr"]
+    spotifyUser = spotifyConfig["spotifyUser"]
 
 #Youtube config stuff
 with open("config/YTconfig.json") as f:
@@ -36,7 +44,12 @@ with open("config/slack.json") as f:
     slackTeam = slackConfig["team"]
     
     slack_client = slack.WebClient(token = slackToken)
-    
+'''
+def auth_spotify():
+    client_credentials_manager = SpotifyClientCredentials(client_id=spotifyClientId, client_secret=spotifyClientSecret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    sp.user_playlist_add_tracks(spotifyUser, spotifyPlaylistId, "spotify:track:1ru5R5iSawvuMELqKXxLjS"); print ("done")
+
 #Function to add song to Spotify playlist
 def add_to_spotify(uri):
     requests.post('https://api.spotify.com/v1/playlists/{}/tracks?position={}&uris={}'.format(spotifyPlaylistId, spotifyCtr, uri),
@@ -51,7 +64,7 @@ def add_to_spotify(uri):
         json.dump({"clientID":spotifyClientID, "bearer":spotifyBearer, "playlistID":spotifyPlaylistId, "ctr":spotifyCtr}, f, indent=4)
     
     print ("Added to Spotify playlist.")
-
+'''
 #Function to add song to Apple Music playlist
 def add_to_apple(songID):
     requests.post(
@@ -90,11 +103,11 @@ def interpret_song(url, user):
         artist = str(applemusic.get('artistName')) #Pulls artist name
         title = str(applemusic.get('title')) #Pulls song name
         
-        add_to_spotify(spotifyID)
-        add_to_apple(applemusicID)
+        addToSpotify(spotifyID)
+        #add_to_apple(applemusicID) Function deprecated
         add_to_youtube(ytAuth(), youtubeID)
 
-        slack_response(("{} by {} was added to the playlist.").format(title, artist), user)
+       # slack_response(("{} by {} was added to the playlist.").format(title, artist), user)
         
         return {
                 "youtubeID": youtubeID, 
@@ -105,27 +118,26 @@ def interpret_song(url, user):
             }
     except:
         slack_response("The song that you linked was not recognized.", user)
+        print ("Song wasn't recognized, or something else broke.")
         return {}
  
 
-@slack.RTMClient.run_on(event="message")
+@slack.RTMClient.run_on(event="message") #Slack listens in pre-defined channel.
 def message_on(**payload):
-    print ("Message sent in channel.")
+    #print ("Message sent in channel.")
     data = payload['data']
     web_client = payload['web_client']
-    
     try: 
         if extractor.has_urls(data['text']):
+            #print ("HAS URLS, CONTINUING.")
             link = list(extractor.find_urls(data['text']))[0]
+            #print ("URLS FOUND, INTERPRETING.")
             interpret_song(link,data['user'])
+            #print ("URLS INTERPRETED AND SENT TO PLATFORMS.")
     except KeyError:
-        print("Text got deleted.")
+        print("Text got deleted, or extractor messed up again.")
     
 slack_token = slackToken #This stuff needs to actually stay here, for slack to listen! Do not delete
 rtm_client = slack.RTMClient(token=slack_token)
 rtm_client.start()
 
-#print (interpret_song('https://music.apple.com/us/album/thunder/1411625594?i=1411629089&app=music&ign-mpt=uo%3D4'))
-
-
-#i love this song' https://music.apple.com/us/album/thunder/1411625594?i=1411629089&app=music&ign-mpt=uo%3D4, it's really amazing and it's good'
---
+++ spotify.py
@@ -0,0 +1,39 @@
+import sys
+import spotipy
+import spotipy.util
+import json
+
+with open("config/SPconfig.json") as f:
+    spotifyConfig = json.load(f)
+    
+    spotifyClientId = spotifyConfig["clientID"]
+    spotifyClientSecret = spotifyConfig["clientSecret"]
+    spotifyBearer = spotifyConfig["bearer"]
+    spotifyPlaylistId = spotifyConfig["playlistID"]
+    spotifyCtr = spotifyConfig["ctr"]
+    spotifyUser = spotifyConfig["spotifyUser"]
+    
+
+
+def addToSpotify(uuid):
+    uid = [uuid] #This converts the UID string into a list, because spotipy api only accepts inputs as lists.
+    
+    scope = 'playlist-modify-public' #Describes the scope necessary, so spotify API can authorize.
+
+    if (len(sys.argv) > 1): #stuff that spotipy uses - leaving it here so stuff doesn't break
+        username = sys.argv[1]
+    else:
+        ("Usage: {} username".format(sys.argv[0])) #This is entirely irrelevant, but the code doesn't run without it.
+
+    token = spotipy.util.prompt_for_user_token(spotifyUser,scope,client_id=spotifyClientId,client_secret=spotifyClientSecret,redirect_uri='http://localhost:9898/spotifyCallback') #Authorizes with Spotify using OAuth
+
+
+    if token: #Adds song to playlist
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False #idk what this does but the docs told me to do it
+        results = sp.user_playlist_add_tracks(spotifyUser, spotifyPlaylistId, uid) #Adds song to said playlist
+        print (results) #logs snapshot to console
+        print ("Added to spotify playlist.")
+    else:
+        print ("CATCH HERE! Couldn't get token.") 
+        
