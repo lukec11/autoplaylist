3fef9c49b705035b2bf8fddc346e18a02f610061
lukec11 <me@lukec.me>
2020-05-12 22:53:50 -0400

    update most of the files for use with captain

--- main.py
+++ main.py
@@ -27,34 +27,36 @@ with open("config/SPconfig.json") as f:
     spotifyUser = spotifyConfig["spotifyUser"]
 
 # Youtube config stuff
-with open("config/YTconfig.json") as f:
-    youtubeConfig = json.load(f)
+youtubeConfig = os.environ
 
-with open("config/slack.json") as f:
-    slackConfig = json.load(f)
-    slackToken = slackConfig["token"]
-    slackChannel = slackConfig["channel"]
-    slackTeam = slackConfig["team"]
 
-slack_client = slack.WebClient(token = slackToken)
+slackConfig = os.environ
+slackToken = slackConfig["slack_token"]
+slackChannel = slackConfig["slack_channel"]
+slackTeam = slackConfig["slack_team"]
+
+slack_client = slack.WebClient(token=slackToken)
 extractor = URLExtract()  # declare extractor for later
 
 # method to post a (parameter) message to slack, visible to channel
+
+
 def slack_response(message, userID):
-    print ("Sending slack response.")
+    print("Sending slack response.")
 
     message = ("{}".format(message))
-    slack_client.chat_postMessage(token = slackToken,
+    slack_client.chat_postMessage(token=slackToken,
                                   as_user=False,
                                   channel=slackChannel,
                                   text=message
-                                 )
+                                  )
 
 
-def slack_ephemeral(message, userID): # method to post an ephemeral message to the chat - only the user will see it
+# method to post an ephemeral message to the chat - only the user will see it
+def slack_ephemeral(message, userID):
 
     message = ("{}".format(message))
-    slack_client.chat_postEphemeral(token = slackToken,
+    slack_client.chat_postEphemeral(token=slackToken,
                                     as_user=False,
                                     channel=slackChannel,
                                     text=message,
@@ -65,50 +67,58 @@ def slack_ephemeral(message, userID): # method to post an ephemeral message to t
 # passes the song info to song.link to get information
 def interpret_song(url, user, origin):
     try:
-        song = requests.get(f'https://api.song.link/v1-alpha.1/links?url={url}').content
+        song = requests.get(
+            f'https://api.song.link/v1-alpha.1/links?url={url}').content
         links = json.loads(song)
 
-        #Access the non-unique platform portions for each, part II of the API
-        spotify = links.get('entitiesByUniqueId').get(links.get('linksByPlatform').get('spotify').get('entityUniqueId')) 
-        youtube = links.get('entitiesByUniqueId').get(links.get('linksByPlatform').get('youtube').get('entityUniqueId'))
-        applemusic = links.get('entitiesByUniqueId').get(links.get('linksByPlatform').get('appleMusic').get('entityUniqueId'))
+        # Access the non-unique platform portions for each, part II of the API
+        spotify = links.get('entitiesByUniqueId').get(
+            links.get('linksByPlatform').get('spotify').get('entityUniqueId'))
+        youtube = links.get('entitiesByUniqueId').get(
+            links.get('linksByPlatform').get('youtube').get('entityUniqueId'))
+        applemusic = links.get('entitiesByUniqueId').get(
+            links.get('linksByPlatform').get('appleMusic').get('entityUniqueId'))
 
-        #Accesses sections fully unique to each part of the ID - Part I
+        # Accesses sections fully unique to each part of the ID - Part I
         youtubeID = str(youtube.get('id'))
-        spotifyID = ("spotify:track:" + str(spotify.get('id'))) 
+        spotifyID = ("spotify:track:" + str(spotify.get('id')))
         applemusicID = str(applemusic.get('id'))
 
-        artist = str(spotify.get('artistName')) #Pulls artist name
-        title = str(applemusic.get('title')) #Pulls song name
+        artist = str(spotify.get('artistName'))  # Pulls artist name
+        title = str(applemusic.get('title'))  # Pulls song name
 
         isDupe = addToSpotify(spotifyID)
-
 
         if not isDupe:
             add_to_youtube(ytAuth(), youtubeID)
         elif isDupe:
-            print (f"INFO: Song {title} is duplicate, not added. [Main]") #logs to console when method is a duplicate
-        
+            # logs to console when method is a duplicate
+            print(f"INFO: Song {title} is duplicate, not added. [Main]")
+
         if not isDupe:
-            slack_response(f" <@{user}> added to the playlist: `{title}` by `{artist}` !", user) #logs publically to slack when message posted
+            # logs publically to slack when message posted
+            slack_response(
+                f" <@{user}> added to the playlist: `{title}` by `{artist}` !", user)
         elif isDupe:
-            slack_ephemeral(f"`{title}` by `{artist}` is already in the playlist!", user) #logs privately to user if song is a duplicate
+            # logs privately to user if song is a duplicate
+            slack_ephemeral(
+                f"`{title}` by `{artist}` is already in the playlist!", user)
         return {
-                "youtubeID": youtubeID, 
-                "spotifyID": spotifyID, 
-                "applemusicID": applemusicID, 
-                "artist": artist, 
-                "title": title
-            }
+            "youtubeID": youtubeID,
+            "spotifyID": spotifyID,
+            "applemusicID": applemusicID,
+            "artist": artist,
+            "title": title
+        }
     except SyntaxError as s:
         slack_ephemeral("The song that you linked was not recognized.", user)
-        print (f"WARNING: SyntaxError - {s}")
-        
+        print(f"WARNING: SyntaxError - {s}")
+
         return {}  # returns empty dictionary
     except AttributeError as a:
-        print (f"WARNING: AttributeError: {a}")
+        print(f"WARNING: AttributeError: {a}")
         slack_ephemeral("Your song was not found! If you've linked many songs recently, please wait 60 seconds, you may be rate-limited. If you didn't link a song, please ignore this message and it will disappear automatically.", user)
-        print ("Rate limited or not found, message posted.")
+        print("Rate limited or not found, message posted.")
 
         return {}  # returns empty dictionary
 
@@ -121,12 +131,14 @@ def message_on(**payload):
     try:
         if extractor.has_urls(data['text']):
             link = list(extractor.find_urls(data['text']))[0]
-            print (f"User printed text: {data['text']}")
+            print(f"User printed text: {data['text']}")
             interpret_song(link, data['user'], 'rtm')
-   
+
     except KeyError as k:
         print(f"WARNING: KeyError from RTM! {k}")
 
-slack_token = slackToken #This stuff needs to actually stay here, for slack to listen! Do not delete
+
+# This stuff needs to actually stay here, for slack to listen! Do not delete
+slack_token = slackToken
 rtm_client = slack.RTMClient(token=slack_token)
 rtm_client.start()
--- spotify.py
+++ spotify.py
@@ -1,4 +1,4 @@
-#Written by Luke Caraprzza (@lukec11) and Harshith Iyer (@harbar20), December 2019
+# Written by Luke Caraprzza (@lukec11) and Harshith Iyer (@harbar20), December 2019
 import sys
 import spotipy
 import spotipy.util
@@ -6,71 +6,78 @@ import json
 
 
 #global vars
-isDuplicate = False #Initializes to false by default, will be set to true below if it is a duplicate
+# Initializes to false by default, will be set to true below if it is a duplicate
+isDuplicate = False
 
-with open("config/SPconfig.json") as f:
-    spotifyConfig = json.load(f)
-    
-    spotifyClientId = spotifyConfig["clientID"]
-    spotifyClientSecret = spotifyConfig["clientSecret"]
-    spotifyBearer = spotifyConfig["bearer"]
-    spotifyPlaylistId = spotifyConfig["playlistID"]
-    spotifyCtr = spotifyConfig["ctr"]
-    spotifyUser = spotifyConfig["spotifyUser"]
+spotifyConfig = os.environ
+
+spotifyClientId = spotifyConfig["spotify_clientID"]
+spotifyClientSecret = spotifyConfig["spotify_clientSecret"]
+spotifyBearer = spotifyConfig["spotify_bearer"]
+spotifyPlaylistId = spotifyConfig["spotify_playlistID"]
+spotifyCtr = spotifyConfig["spotify_ctr"]
+spotifyUser = spotifyConfig["spotify_spotifyUser"]
+
 
 def spDupeChecker(tracks, uuid):
-    for trackInfo in tracks["items"]: #Loops through all the tracks to check if the inputted track is already in the playlist
+    # Loops through all the tracks to check if the inputted track is already in the playlist
+    for trackInfo in tracks["items"]:
         id = "spotify:track:"+trackInfo["track"]["id"]
         if uuid == id:
-            return True #returns true if it finds a duplicate while looping through the playlist
+            return True  # returns true if it finds a duplicate while looping through the playlist
         else:
             alreadyInPlaylist = False
-        
-    return alreadyInPlaylist #Returns False if alreadyInPlaylist hasn't been returned to be true
+
+    # Returns False if alreadyInPlaylist hasn't been returned to be true
+    return alreadyInPlaylist
+
 
 def addToSpotify(uuid):
-    uid = [uuid] #This converts the UID string into a list, because spotipy api only accepts inputs as lists.
-    
+    # This converts the UID string into a list, because spotipy api only accepts inputs as lists.
+    uid = [uuid]
+
     #global vars
     global isDuplicate
-    
-    scope = 'playlist-modify-public' #Describes the scope necessary, so spotify API can authorize.
 
-    if (len(sys.argv) > 1): #stuff that spotipy uses - leaving it here so stuff doesn't break
+    # Describes the scope necessary, so spotify API can authorize.
+    scope = 'playlist-modify-public'
+
+    if (len(sys.argv) > 1):  # stuff that spotipy uses - leaving it here so stuff doesn't break
         username = sys.argv[1]
     else:
-        ("Usage: {} username".format(sys.argv[0])) #This is entirely irrelevant, but the code doesn't run without it.
+        # This is entirely irrelevant, but the code doesn't run without it.
+        ("Usage: {} username".format(sys.argv[0]))
 
-    token = spotipy.util.prompt_for_user_token(spotifyUser, #prompts for spotify user token, so it can access the api
+    token = spotipy.util.prompt_for_user_token(spotifyUser,  # prompts for spotify user token, so it can access the api
                                                scope,
                                                client_id=spotifyClientId,
                                                client_secret=spotifyClientSecret,
                                                redirect_uri='http://localhost:9898/spotifyCallback'
-                                               ) #Authorizes with Spotify using OAuth
+                                               )  # Authorizes with Spotify using OAuth
 
-    if token: #Adds song to playlist
+    if token:  # Adds song to playlist
         sp = spotipy.Spotify(auth=token)
-        sp.trace = False #idk what this does but the docs told me to do it
-        
-        tracks = sp.user_playlist_tracks(spotifyUser, spotifyPlaylistId, limit=100) #Gets all tracks in the playlist
-        
-        alreadyInPlaylist = spDupeChecker(tracks, uuid) #Checks for duplicate tracks in the playlist
+        sp.trace = False  # idk what this does but the docs told me to do it
+
+        # Gets all tracks in the playlist
+        tracks = sp.user_playlist_tracks(
+            spotifyUser, spotifyPlaylistId, limit=100)
+
+        # Checks for duplicate tracks in the playlist
+        alreadyInPlaylist = spDupeChecker(tracks, uuid)
         if not alreadyInPlaylist:
-            results = sp.user_playlist_add_tracks(spotifyUser, 
-                                              spotifyPlaylistId, 
-                                              uid
-                                              ) #Adds song to said playlist
-            #print (results) #logs snapshot to console
-            print ("Added to spotify playlist.")
+            results = sp.user_playlist_add_tracks(spotifyUser,
+                                                  spotifyPlaylistId,
+                                                  uid
+                                                  )  # Adds song to said playlist
+            # print (results) #logs snapshot to console
+            print("Added to spotify playlist.")
             isDuplicate = False
         else:
             print("I'm sorry, this song is already in the playlist.")
             isDuplicate = True
-            
-            
-    else:
-        print ("CATCH HERE! Couldn't get token.") 
-    
-    return isDuplicate
 
-#addToSpotify("spotify:track:2g2a5kDeZexbUTD8abcvm6")
+    else:
+        print("CATCH HERE! Couldn't get token.")
+
+    return isDuplicate
